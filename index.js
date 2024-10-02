20834ca2285c39a1760cb4d37ebb26e5e0dc030c
chris <Mileschris466@gmail.com>
2024-10-02 10:33:23 +0300

    scope declaring

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+};
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+};
+
+function overwriteBestCustomer(newBestCustomer) {
+  bestCustomer = newBestCustomer;
+};
+
+const leastFavoriteCustomer = 'john'
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'unsuccessfully tries to reassign the least favourite customer ';
+};
